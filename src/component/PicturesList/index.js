a6e3fe746895ca9a30f4a6402d3ce87089fd2a27
Breno Mazieiro <breno.mazieiro@brenomazieiro.lan>
2020-04-04 18:36:56 +0100

    PictureList will receive modal

--- src/component/PicturesList/index.js
+++ src/component/PicturesList/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardPicture from '../CardPicture'
+import PictureModal from '../PictureModal'
 
-import './styles.css';
+import './styles.scss';
 
 export default function FotoList({ pictures, setPage, page }) {
+  const [actualPicture, setActualPicture] = useState({})
   return (
     <>
+      <PictureModal actualPicture={actualPicture} setActualPicture={setActualPicture}/>
       <div id="FootList">
         {
           pictures.map((pic) => {
@@ -14,10 +17,12 @@ export default function FotoList({ pictures, setPage, page }) {
               title={pic.title}
               img={'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg'}
               author={pic.ownername}
+              postedDate={pic.dateupload}
+              setActualPicture={setActualPicture}
             />
           })
         }
-      </div>
+      </div>     
       <div id="loadmore" onClick={() => setPage(page + 1)}>Load more...</div>
     </>
   );
