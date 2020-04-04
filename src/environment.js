7839bbfb76f08ab5c50516fffa9e5a4a7ace7f1b
Breno Mazieiro <breno.mazieiro@brenomazieiro.lan>
2020-04-04 17:30:45 +0100

    Adding environment vars

---
+++ src/environment.js
@@ -0,0 +1,3 @@
+export const FLICKR_URL = 'https://api.flickr.com/services/rest?method=flickr.photos.search'
+export const API_KEY = 'ef1f9d4f8ca80dada31c684364355282'
+export const FLICKR_SIG = 'd7f57fa9e01a6a2d6ccd8597b8d2f86b'
