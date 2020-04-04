78ba15852a73e662d531ae890c10edc474687463
Breno Mazieiro <breno.mazieiro@brenomazieiro.lan>
2020-04-04 17:32:01 +0100

    Adding SearchBar component

---
+++ src/component/SearchBar/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import './styles.scss';
+
+export default function SearchBar({ setSearch, setPage, page }) {
+  const [open, setOpen] = useState(false)
+  const focusUsernameInputField = input => {
+    if (input && open && page === 0) {
+      input.focus()
+    }
+  };
+  return (
+    <div className={`search ${open && 'open'}`}>
+      <input
+        ref={focusUsernameInputField}
+        type="search"
+        onChange={(e) => {
+          setPage(0)
+          setSearch(e.target.value || 'dog')
+        }} 
+        className="search-box"
+      />
+      <span
+        className="search-button"
+        onClick={() => setOpen(!open)}
+      >
+        <span className="search-icon"></span>
+      </span>
+    </div>
+  );
+}
